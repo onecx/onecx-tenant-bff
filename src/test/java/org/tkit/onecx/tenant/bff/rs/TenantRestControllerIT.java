f20ef72ec8ff033f207cf6128b2fad98cde12bfd
jsteenke <146953549+jsteenke@users.noreply.github.com>
2024-03-19 14:51:01 +0100

    feat: initial setup bff (#1)

---
+++ src/test/java/org/tkit/onecx/tenant/bff/rs/TenantRestControllerIT.java
@@ -0,0 +1,8 @@
+package org.tkit.onecx.tenant.bff.rs;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class TenantRestControllerIT extends TenantRestControllerTest {
+
+}
