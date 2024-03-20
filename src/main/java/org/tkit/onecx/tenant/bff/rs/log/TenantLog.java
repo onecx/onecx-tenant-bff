be593af5df534303e4273673fc35f751d8f85daf
Jannik Steenken <jannik.steenken@capgemini.com>
2024-03-20 15:05:46 +0100

    fix: tenantlog fix

--- src/main/java/org/tkit/onecx/tenant/bff/rs/log/TenantLog.java
+++ src/main/java/org/tkit/onecx/tenant/bff/rs/log/TenantLog.java
@@ -33,22 +33,6 @@ public class TenantLog implements LogParam {
                                 ", pageSize:" +
                                 ((TenantSearchCriteriaDTO) x).getPageSize()
                                 + "]"),
-                this.item(10, TenantSearchCriteria.class,
-                        x -> "TenantSearchCriteria[orgId:" +
-                                ((TenantSearchCriteria) x).getOrgId() +
-                                ", pageNumber:" +
-                                ((TenantSearchCriteria) x).getPageNumber() +
-                                ", pageSize:" +
-                                ((TenantSearchCriteria) x).getPageSize()
-                                + "]"),
-                this.item(10, TenantPageResultDTO.class,
-                        x -> "TenantPageResultDTO[pageNumber:" +
-                                ((TenantPageResultDTO) x).getTotalPages()
-                                + ", stream:" +
-                                ((TenantPageResultDTO) x).getStream().toString()
-                                + ", pageSize:" +
-                                ((TenantPageResultDTO) x).getSize()
-                                + "]"),
                 this.item(10, UpdateTenantRequestDTO.class,
                         x -> "UpdateTenantRequestDTO[orgId:" +
                                 ((UpdateTenantRequestDTO) x).getOrgId()
