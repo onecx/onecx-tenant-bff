2db0074a4b0a4d3ac7c4d04891e696c17efbc98e
jsteenke <146953549+jsteenke@users.noreply.github.com>
2024-03-21 11:15:49 +0100

    fix. tenant log fixed (#6)
    * feat: initial setup bff
    
    * fix: sonar fixes
    
    * fix: sonar fixes
    
    * fix: tenantlog fix
    
    * fix: tenantlog fix

--- src/main/java/org/tkit/onecx/tenant/bff/rs/log/TenantLog.java
+++ src/main/java/org/tkit/onecx/tenant/bff/rs/log/TenantLog.java
@@ -7,10 +7,8 @@ import jakarta.enterprise.context.ApplicationScoped;
 import org.tkit.quarkus.log.cdi.LogParam;
 
 import gen.org.tkit.onecx.tenant.bff.rs.internal.model.CreateTenantRequestDTO;
-import gen.org.tkit.onecx.tenant.bff.rs.internal.model.TenantPageResultDTO;
 import gen.org.tkit.onecx.tenant.bff.rs.internal.model.TenantSearchCriteriaDTO;
 import gen.org.tkit.onecx.tenant.bff.rs.internal.model.UpdateTenantRequestDTO;
-import gen.org.tkit.onecx.tenant.client.model.TenantSearchCriteria;
 
 @ApplicationScoped
 public class TenantLog implements LogParam {
@@ -32,22 +30,6 @@ public class TenantLog implements LogParam {
                                 ((TenantSearchCriteriaDTO) x).getPageNumber() +
                                 ", pageSize:" +
                                 ((TenantSearchCriteriaDTO) x).getPageSize()
-                                + "]"),
-                this.item(10, TenantSearchCriteria.class,
-                        x -> "TenantSearchCriteria[orgId:" +
-                                ((TenantSearchCriteria) x).getOrgId() +
-                                ", pageNumber:" +
-                                ((TenantSearchCriteria) x).getPageNumber() +
-                                ", pageSize:" +
-                                ((TenantSearchCriteria) x).getPageSize()
-                                + "]"),
-                this.item(10, TenantPageResultDTO.class,
-                        x -> "TenantPageResultDTO[pageNumber:" +
-                                ((TenantPageResultDTO) x).getTotalPages()
-                                + ", stream:" +
-                                ((TenantPageResultDTO) x).getStream().toString()
-                                + ", pageSize:" +
-                                ((TenantPageResultDTO) x).getSize()
                                 + "]"),
                 this.item(10, UpdateTenantRequestDTO.class,
                         x -> "UpdateTenantRequestDTO[orgId:" +
